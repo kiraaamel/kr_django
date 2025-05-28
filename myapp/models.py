3603513eb53df3170e2d6ccd8a6a99d49f02da37
kiraaamel <kiraaamel@gmail.com>
2025-05-28 10:50:27 +0300

    Модель kmexam

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class kmexam(models.Model):
+    title = models.CharField("Название экзамена", max_length=255)
+    created_at = models.DateTimeField("Дата создания записи", auto_now_add=True)
+    exam_date = models.DateField("Дата проведения экзамена")
+    image = models.ImageField("Задание по экзамену в виде картинки", upload_to='exam_images/')
+    users = models.ManyToManyField(User, related_name='exams', verbose_name="Пользователи, который пишут экзамен")
+    is_public = models.BooleanField("Опубликовано", default=False)
+
+    def __str__(self):
+        return self.title
+
